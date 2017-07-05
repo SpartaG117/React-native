dc98042d4b9be24f9f5bf0dd705fb177dfb2beb3
SpartaG117 <444219104@qq.com>
2017-07-05 15:56:56 +0800

    项目初始化

---
+++ hello/index.android.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class hello extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to SPART117's homework!!
+        </Text>
+        <Text style={styles.instructions}>
+          希望给个通过
+        </Text>
+        <Text style={styles.instructions}>
+          谢谢老师！！！
+        </Text>
+        <Text style={styles.instructions}>
+          姓名：甘卓欣
+        </Text><Text style={styles.instructions}>
+          学号：201622060104
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('hello', () => hello);
